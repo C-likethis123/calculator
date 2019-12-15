a97b042849c34e5e958a549b23d1b71e575acfde
Chow Jia Ying <chowjiaying211@gmail.com>
2019-12-15 12:40:31 +0800

    Adds a script with basic arithmetic functions

---
+++ script.js
@@ -0,0 +1,16 @@
+//Basic arithmetic functions
+function add(op1, op2) {
+	return op1 + op2;
+}
+
+function subtract(op1, op2) {
+	return op1 - op2;
+}
+
+function multiply(op1, op2) {
+	return op1 - op2;
+}
+
+function divide(op1, op2) {
+	return op1 / op2;
+}
