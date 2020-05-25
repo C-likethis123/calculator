d33c56a0799ccfe31503eb80ffdc3f2ca46d2757
Chow Jia Ying <chowjiaying211@gmail.com>
2020-05-26 01:08:07 +0800

    Add functionality for delete button

--- script.js
+++ script.js
@@ -20,3 +20,13 @@ function clearScreen() {
 
 const clearButton = document.querySelector("button[data-function=clear");
 clearButton.addEventListener("click", clearScreen);
+
+// Delete button deletes last character
+function deleteFromScreen() {
+  const currentDisplay = screen.textContent;
+  const newDisplay = currentDisplay.substring(0, currentDisplay.length - 1);
+  screen.textContent = newDisplay;
+}
+
+const deleteButton = document.querySelector("button[data-function=delete");
+deleteButton.addEventListener("click", deleteFromScreen);
